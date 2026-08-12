9b7a22cdfa2eace3271522f50e41264bc7bfdc74
faresx7 <faresesaam001@gmail.com>
2026-08-12 21:34:24 +0300

    feat: Add Flask server setup with chat endpoint and error handling

--- AI model/server.py
+++ AI model/server.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import base as b
+
+app = Flask(__name__)
+print('===========================================================model is preparing===========================================================')
+model = b.AIModel()
+print('===========================================================model is ready...===========================================================')
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/chat', methods = ["POST"])
+def generate():
+
+    try:
+        data = request.get_json()
+
+        if not data or "prompt" not in data:
+            return jsonify({'error':'No message have been sent!'}), 400
+
+        prompt = data['prompt']
+        model_response = model.generate(prompt)
+
+        return jsonify({'response':model_response})
+
+    except Exception as e: 
+        return jsonify({'error':str(e)}), 500
 
 
-# @app.route('/')
-# def 
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000, debug=True, use_reloader = False)
