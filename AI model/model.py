511eb4e720e06fe664d579abade9a6ab8360c392
faresx7 <faresesaam001@gmail.com>
2026-08-20 16:36:21 +0300

    feat(model): add generate_stream_response method for streaming responses
    - Keep standard generate_response intact for full responses

--- AI model/model.py
+++ AI model/model.py
@@ -4,10 +4,12 @@ from threading import Thread
 
 
 class AIModel:
-    def __init__(self, sys_prompt = '''Your name is Mr.Roberto.
+    def __init__(self, 
+                 sys_prompt = '''Your name is Mr.Roberto.
                                     You are a sharp, smart, and friendly human.
                                     You pay close attention to details, stay calm,
-                                    and talk naturally without overreacting.'''):
+                                    and talk naturally without overreacting.'''
+                                    ):
 
         self.model_name = "Qwen/Qwen2.5-1.5B-Instruct"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
@@ -17,6 +19,7 @@ class AIModel:
                                                     device_map="auto")
 
         self.chat_history = [{'role':'system','content':sys_prompt}]
+
 
     def generate_response(self, prompt):
         self.chat_history.append({"role": "user","content": prompt})
@@ -35,18 +38,55 @@ class AIModel:
                     ).to(self.model.device)
 
 
+        with torch.inference_mode():
+            outputs = self.model.generate(**inputs,  #type:ignore
+                                        max_new_tokens=500,
+                                        temperature = .6,             #controls the creativity of the model  
+                                        top_p =.9,                    #get the most 90% related to the subject and ignore others 
+                                        #   top_k = 50,                   # same as top_p but don't use the percentage
+                                        repetition_penalty = 1.1,      # to reduce repetition of words
+                                            )
+
+
+        output_message = self.tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:],
+                                            skip_special_tokens = True)
+
+
+        self.chat_history.append({'role':'assistant','content':output_message})
+
+        return output_message
+
+
+
+    def generate_stream_response(self, prompt):
+        self.chat_history.append({"role": "user","content": prompt})
+
+        if len(self.chat_history) > 7:
+            self.chat_history[:] = [self.chat_history[0]] + self.chat_history[-6:]
+
+        message = self.chat_history
+        
+        inputs = self.tokenizer.apply_chat_template(
+                message,
+                add_generation_prompt=True,
+                tokenize=True,
+                return_dict=True,
+                return_tensors="pt",
+                    ).to(self.model.device)
+
+        
         # == this section is how to stream text into UI == 
         streamer = TextIteratorStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
 
         generate_kwargs = dict(
             **inputs,  #type:ignore
-            max_new_tokens=500,
-            temperature = .6,             #controls the creativity of the model  
-            top_p =.9,                    #get the most 90% related to the subject and ignore others 
-            #top_k = 50,                   # same as top_p but don't use the percentage
-            repetition_penalty = 1.1,      # to reduce repetition of words
+            max_new_tokens = 500,
+            temperature = .6,
+            top_p = .9, 
+            #top_k = 50,                   
+            repetition_penalty = 1.1,      
             streamer = streamer
-                        )
+            )
 
         thread = Thread(target=self.model.generate, kwargs=generate_kwargs) #type:ignore
         thread.start()
@@ -55,10 +95,3 @@ class AIModel:
         for new_text in streamer:
             full_response += new_text
             yield new_text
-
-
-        self.chat_history.append({'role':'assistant','content':full_response})
-
-
-
-
